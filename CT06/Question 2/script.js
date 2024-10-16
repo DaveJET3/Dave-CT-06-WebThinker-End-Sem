6b9e3a6d82acb217059e89516d5d76bac12f2050
DaveJET3 <chowge@gmail.com>
2024-10-16 08:41:23 +0000

    Oct 16, 2024, 8:41 AM

---
+++ CT06/Question 2/script.js
@@ -0,0 +1,14 @@
+<head>
+    <body>
+        const radius=()={
+            console.log("radius")
+        } 
+        const PI=()={
+            console.log("PI")
+        }   
+        const area_circle=()={
+            console.log("area_circle")
+        }
+        area_circle(PI * radius * radius);
+    </body>
+</head>
